e0aeabee2555f51e48d04a9522e3a31dd9d7d791
wangaevans <wangaevans84@gmail.com>
2023-10-27 12:12:15 +0300

    + hamburger menu

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,6 +1,8 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 import ThemeToggle from '../theme/theme-toggle'
 import Link from 'next/link'
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai'
 
 interface NavbarProps {
   links: {
@@ -16,20 +18,32 @@ interface NavbarProps {
 }
 
 export default function Navbar({ links, branding }: NavbarProps) {
+  const [isOpen, setIsOpen] = useState(false)
+  const handleClick = () => {
+    if (!isOpen) setIsOpen(true)
+    else setIsOpen(!true)
+  }
   return (
-    <header className="h-20 dark:bg-primary-950 bg-primary-100">
-      <nav className="container flex h-full justify-between items-center  text-primary-900 transition-all  dark:text-primary-200 px-4 md:px-2">
+    <header className="h-20 bg-primary-100 dark:bg-primary-950">
+      <nav className="container flex h-full items-center justify-between  px-4 text-primary-900  transition-all dark:text-primary-200 md:px-2">
         <Link href="/" className="text-2xl font-semibold">
           {branding.name}
         </Link>
-        <ul className="hidden md:flex items-center space-x-5">
+        <ul className={`${isOpen?"top-[90px] h-1/2 w-1/2":""} absolute -top-full md:top-[initial]  block items-center md:space-x-5 md:relative md:flex`}>
           {links.map((link, index) => (
             <li key={index}>
-              <Link className='text-[1.1rem]' href={link.href}>{link.title}</Link>
+              <Link className="text-[1.1rem]" href={link.href}>
+                {link.title}
+              </Link>
             </li>
           ))}
         </ul>
-        <ThemeToggle className='text-xl mr-4'  />
+        <div className='flex space-x-4'>
+        <ThemeToggle className="mr-4 text-xl" />
+        <button className="block md:hidden text-2xl" onClick={handleClick}>
+          {isOpen ? <AiOutlineClose /> : <AiOutlineMenu />}
+        </button>
+        </div>
       </nav>
     </header>
   )
