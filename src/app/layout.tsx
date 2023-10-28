657fb549570c468d449c1b998e4e749457a785da
wangaevans <wangaevans84@gmail.com>
2023-10-28 22:43:22 +0300

    + g analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,13 +5,16 @@ import Navbar from '../components/navbar'
 import ThemeProvider from '../components/theme/theme-provider'
 import Footer from '../components/footer'
 import { Metadata } from 'next'
+import { Suspense } from 'react'
+import Analytics from '../components/analytics'
+import GoogleAnalytics from './GoogleAnalytics'
 const commissioner = Commissioner({ subsets: ['latin'] })
 
-export const metadata:Metadata = {
+export const metadata: Metadata = {
   title: config.site.branding.name,
   description: config.site.branding.description,
-  icons:{
-    icon:"/favicon.ico"
+  icons: {
+    icon: '/favicon.ico'
   }
 }
 interface RootLayoutProps {
@@ -21,11 +24,16 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${commissioner.className}`}>
+        <Suspense>
+          <Analytics />
+        </Suspense>
+          <GoogleAnalytics/>
         <ThemeProvider>
           <Navbar branding={config.site.branding} links={config.site.links} />
           {children}
           <Footer copyright={config.site.copyright} />
         </ThemeProvider>
+     
       </body>
     </html>
   )
