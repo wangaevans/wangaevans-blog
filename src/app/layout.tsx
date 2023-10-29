ae073f54b1841904efdc49fdabcc2c8d33f87756
wangaevans <wangaevans84@gmail.com>
2023-10-29 08:01:25 +0300

    + g analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,9 +5,8 @@ import Navbar from '../components/navbar'
 import ThemeProvider from '../components/theme/theme-provider'
 import Footer from '../components/footer'
 import { Metadata } from 'next'
-import Analytics from 'analytics'
-import googleTagManager from '@analytics/google-tag-manager'
 import GoogleAnalytics from './GoogleAnalytics'
+import Script from 'next/script'
 const commissioner = Commissioner({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -21,19 +20,17 @@ interface RootLayoutProps {
   children: React.ReactNode
 }
 export default function RootLayout({ children }: RootLayoutProps) {
-  const analytics = Analytics({
-    app: 'wangaevans.com',
-    plugins: [
-      googleTagManager({
-        containerId: process.env.NEXT_PUBLIC_GTM
-      })
-    ]
-  })
-  
-  /* Track a page view */
-  analytics.page()
   return (
     <html lang="en" suppressHydrationWarning>
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-WPL65WDW');
+        `}
+    </Script>
       <body className={`${commissioner.className}`}>
        
           <GoogleAnalytics/>
@@ -42,7 +39,11 @@ export default function RootLayout({ children }: RootLayoutProps) {
           {children}
           <Footer copyright={config.site.copyright} />
         </ThemeProvider>
-     
+        <noscript
+        dangerouslySetInnerHTML={{
+        __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WPL65WDW" height="0" width="0" style="display: none; visibility: hidden;"></iframe>`,
+        }} 
+    />
       </body>
     </html>
   )
