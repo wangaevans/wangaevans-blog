24d1931de4c444d3f1bc7f6ec80fcdbaa03ec386
wangaevans <wangaevans84@gmail.com>
2023-10-30 05:58:07 +0300

    + clipboard cpy

--- src/components/Pre.jsx
+++ src/components/Pre.jsx
@@ -7,7 +7,7 @@ export default function Pre({ children, code, ...props }) {
   return (
     <pre {...props} className='p-0 rounded-lg border border-primary-400 dark:border-primary-900'>
       <div className='code-header p-0   bg-gray-900'>
-        <div className='px-4 py-2 text-primary-500 flex justify-between'>
+        <div className='px-4 py-1 md:py-2  text-primary-500 flex justify-between'>
           {lang}
           <CopyButton text={code} />
         </div>
