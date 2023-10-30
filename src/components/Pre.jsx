c9dccd2ec8b3d5f0bd26feeff079a9fe0ac7062d
wangaevans <wangaevans84@gmail.com>
2023-10-30 06:59:14 +0300

    + clipboard cpy

--- src/components/Pre.jsx
+++ src/components/Pre.jsx
@@ -2,18 +2,18 @@ import React  from 'react'
 import CopyButton from "./copy-button"
 
 
-export default function Pre({ children, code, ...props }) {
+export default function Pre(props) {
   const lang = props['data-language']
   return (
     <pre {...props} className='p-0 rounded-lg border border-primary-400 dark:border-primary-900'>
       <div className='code-header p-0   bg-gray-900'>
         <div className='px-4 py-1 md:py-2  text-primary-500 flex justify-between'>
           {lang}
-          <CopyButton text={code} />
+          <CopyButton text={props.raw} />
         </div>
       </div>
       <div className="py-4 px-4">
-        {children}
+        {props.children}
       </div>
     </pre>
   )
